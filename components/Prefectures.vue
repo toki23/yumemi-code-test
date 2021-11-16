5f184fea38d5a56c7b8f6c1226cf836f8fccb2f8
鷺空汰 <blancshio@gmail.com>
2021-11-17 00:25:54 +0900

    チェックボックスの間隔が統一されるように修正

--- components/Prefectures.vue
+++ components/Prefectures.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
     <p>都道府県</p>
-
-    <span v-for="prefecture in prefectures" :key="prefecture.prefCode">
-      <input
-        :id="prefecture.prefCode"
-        v-model="prefecture.selected"
-        type="checkbox"
-        :name="prefecture.prefCode"
-      />
-      <label :for="prefecture.prefCode">{{ prefecture.prefName }}</label>
-    </span>
+    <div class="checkbox-container">
+      <span
+        class="checkbox"
+        v-for="prefecture in prefectures"
+        :key="prefecture.prefCode"
+      >
+        <input
+          :id="prefecture.prefCode"
+          v-model="prefecture.selected"
+          type="checkbox"
+          :name="prefecture.prefCode"
+        />
+        <label :for="prefecture.prefCode">{{ prefecture.prefName }}</label>
+      </span>
+    </div>
   </div>
 </template>
 <script>
@@ -23,3 +28,14 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.checkbox-container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+.checkbox {
+  width: 90px;
+}
+</style>
