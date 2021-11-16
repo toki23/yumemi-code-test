c31f3d3f725b91c16878169dc88701111cdaf146
toki23 <blancshio@gmail.com>
2021-11-16 21:50:58 +0900

    チェックボックスの配置

--- components/Prefectures.vue
+++ components/Prefectures.vue
@@ -1,3 +1,23 @@
 <template>
+  <div>
     <p>都道府県</p>
+
+    <span v-for="prefecture in prefectures" :key="prefecture.prefCode">
+      <input
+        type="checkbox"
+        :id="prefecture.prefCode"
+        :name="prefecture.prefCode"
+      />
+      <label :for="prefecture.prefCode">{{ prefecture.prefName }}</label>
+    </span>
+  </div>
 </template>
+<script>
+export default {
+  props: ['prefectures'],
+
+  mounted() {
+    console.log(this.prefectures)
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
     <Header />
-    <Prefectures />
+    <Prefectures :prefectures="this.prefectures" />
     <Graph />
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  mounted() {},
+  async asyncData({ $axios, $config }) {
+    const prefectures = await $axios.get(
+      'https://opendata.resas-portal.go.jp/api/v1/prefectures',
+      {
+        headers: { 'X-API-KEY': $config.apiKey },
+      }
+    )
+    console.log(prefectures.data.result)
+    return { prefectures: prefectures.data.result }
+  },
+}
 </script>
